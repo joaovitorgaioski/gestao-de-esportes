aff76d9dabf63f0b37542a36347190bb58cb8cea
miguelkoloda <mateuskoloda@gmail.com>
2026-08-24 14:54:06 -0300

    feat: corrige mensagem de saudação e adiciona configuração de tipos no tsconfig

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,10 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-<<<<<<< HEAD
     return 'Hello mundo!';
-=======
-    return 'Hello World!';
->>>>>>> f2bb91c (feat: inicia backend com NestJS e Prisma)
   }
 }
